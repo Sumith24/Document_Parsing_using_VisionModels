9aa3b5646d8bf04ff1783ee26c7539b07b8bd30e
Sumith Jeevapur <sumith.jeevapur@version1.com>
2025-02-23 23:34:14 +0530

    added report

--- Donut/main.py
+++ Donut/main.py
@@ -13,43 +13,6 @@ model = VisionEncoderDecoderModel.from_pretrained(model_name)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
-def get_evaluations(ground_truth, extracted_path):
-    """
-    Compute accuracy using similarity.
-    """
-    c_name_count = 0
-    t_price_count = 0
-    total = 0
-    time_tkn = list()
-    for parsed_file in os.listdir(extracted_path):
-        for grwdth_file in os.listdir(ground_truth):
-            if grwdth_file.endswith(".json"):
-                if os.path.splitext(parsed_file)[0].split("_")[-1]==grwdth_file.split("_")[0]:
-                    total+=1
-                    with open(os.path.join(extracted_path, parsed_file), "r") as pf:
-                        p_data = json.load(pf)
-                        if isinstance(p_data["menu"], list):
-                            parsed_company_name = p_data["menu"][0]["nm"]
-                        else:
-                            parsed_company_name = p_data["menu"]["nm"]
-                        parsed_total_price = p_data["total"]["total_price"]
-                    with open(os.path.join(ground_truth, grwdth_file), "r") as gf:
-                        g_data = json.load(gf)
-                        ground_company_name = g_data["Company_Name"]
-                        ground_total_price = g_data["total_price"]
-                    if parsed_company_name==ground_company_name:
-                        c_name_count += 1
-                    if parsed_total_price==ground_total_price:
-                        t_price_count += 1
-                    time_tkn.append(p_data["processing_time"])
-
-    avg_processing_time = sum(time_tkn)/len(time_tkn)
-    print(f"Out of {total} images, Model extracted {c_name_count}-Company_Name correctly")
-    print(f"Out of {total} images, Model extracted {t_price_count}-total_price correctly")
-    print(f"Average time taken to process a single image is {round(avg_processing_time, 2)} sec")
-    accuarcy = (c_name_count+t_price_count)/(2*total)
-    print(f"\nModel accuracy is {round(accuarcy, 2)}")
-
 def parse_receipt_data(image_path):
     """
     Extract structured data from a receipt image using Donut model.
@@ -86,6 +49,43 @@ def parse_receipt_data(image_path):
     
     return result
 
+def get_evaluations(ground_truth, extracted_path):
+    """
+    Compute accuracy using similarity.
+    """
+    c_name_count = 0
+    t_price_count = 0
+    total = 0
+    time_tkn = list()
+    for parsed_file in os.listdir(extracted_path):
+        for grwdth_file in os.listdir(ground_truth):
+            if grwdth_file.endswith(".json"):
+                if os.path.splitext(parsed_file)[0].split("_")[-1]==grwdth_file.split("_")[0]:
+                    total+=1
+                    with open(os.path.join(extracted_path, parsed_file), "r") as pf:
+                        p_data = json.load(pf)
+                        if isinstance(p_data["menu"], list):
+                            parsed_company_name = p_data["menu"][0]["nm"]
+                        else:
+                            parsed_company_name = p_data["menu"]["nm"]
+                        parsed_total_price = p_data["total"]["total_price"]
+                    with open(os.path.join(ground_truth, grwdth_file), "r") as gf:
+                        g_data = json.load(gf)
+                        ground_company_name = g_data["Company_Name"]
+                        ground_total_price = g_data["total_price"]
+                    if parsed_company_name==ground_company_name:
+                        c_name_count += 1
+                    if parsed_total_price==ground_total_price:
+                        t_price_count += 1
+                    time_tkn.append(p_data["processing_time"])
+
+    avg_processing_time = sum(time_tkn)/len(time_tkn)
+    print(f"\nOut of {total} images, Model extracted {c_name_count}-Company_Name correctly")
+    print(f"Out of {total} images, Model extracted {t_price_count}-total_price correctly")
+    print(f"Average time taken to process a single image is {round(avg_processing_time, 2)} sec")
+    accuarcy = (c_name_count+t_price_count)/(2*total)
+    print(f"\nModel accuracy is {round(accuarcy, 2)}")
+
 def process_receipts(input_folder, output_path):
     """
     Process all receipt images in a folder and save structured output.
@@ -96,12 +96,11 @@ def process_receipts(input_folder, output_path):
             output_file = os.path.join(output_path,"parsed_json_"+os.path.basename(file).split(".")[0]+".json")
             with open(output_file, "w") as f:
                 json.dump(receipt_data, f, indent=4)
-
             print(f"Extraction completed. Results saved in {output_file}")
 
 if __name__ == "__main__":
     input_folder = "../Image_data/"
     output_path = "output/"
-    process_receipts(input_folder, output_path)
+    # process_receipts(input_folder, output_path)
     get_evaluations(input_folder, output_path)
 
